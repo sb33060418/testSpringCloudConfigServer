f38808e0a41c7e18126b57234f1d7b02bf01047a
sunbin <sunbin@windows10.microdone.cn>
2019-05-25 17:52:06 +0800

    1

--- src/main/java/com/sunbin/test/spring/cloud/config/server/TestSpringCloudConfigServerApplication.java
+++ src/main/java/com/sunbin/test/spring/cloud/config/server/TestSpringCloudConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.sunbin.test.spring.cloud.config;
+package com.sunbin.test.spring.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
